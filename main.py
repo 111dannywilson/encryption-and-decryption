fdd823d3b9d694a87e5d33691ef4898965de5dd2
Danny Wilson <111dannywilson@gmail.com>
2023-10-09 02:34:55 -1200

    no info

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def encrypt_data(text: str) -> str:
     encrypted_text = ""
     for i in str(text).lower():
         encrypting = encryption_bp.find(i)
-        encrypted_text += encryption_bp[encrypting + 1] if i != "9" else encryption_bp[0]
+        encrypted_text += encryption_bp[encrypting + 1] if i != encryption_bp[-1] else encryption_bp[0]
     return encrypted_text
 
 
@@ -25,11 +25,11 @@ def decrypt_data(encrypted_text: str) -> str:
     decrypted_text = ""
     for i in str(encrypted_text).lower():
         decrypting = encryption_bp.find(i)
-        decrypted_text += encryption_bp[decrypting - 1] if i != "a" else " "
+        decrypted_text += encryption_bp[decrypting - 1] if i != encryption_bp[0] else " "
     return decrypted_text
 
 
-encryption_bp = ascii_lowercase + digits
+encryption_bp = ascii_lowercase + digits + "#"
 
 encrypt = encrypt_data("cat")
 decrypt = decrypt_data("dbu")
